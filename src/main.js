dca8544930f702db59c9d0cff999fc52db1351c0
Lewis Wilson <lewiswilson1998@hotmail.co.uk>
2026-03-30 17:27:12 +0100

    fix basic example

--- src/main.js
+++ src/main.js
@@ -20,9 +20,7 @@ const vertexShader = `
 `;
 
 const fragmentShader = `
-  #ifdef GL_ES
   precision mediump float;
-  #endif
 
   uniform float u_time;
   uniform vec2 u_res;
@@ -38,13 +36,18 @@ const fragmentShader = `
 
   void main() {
     Metaball metaballs[4];
-    metaballs[0] = Metaball(vec3(1,0,0), vec2(100.0,90.0), 60.0);
-    metaballs[1] = Metaball(vec3(0,1,0), vec2(120.0,200.0), 50.0);
-    metaballs[2] = Metaball(vec3(0,0,1), vec2(200.0,90.0), 45.0);
-    metaballs[3] = Metaball(vec3(1,0,0), vec2(400.0,190.0), 45.0);
+    metaballs[0] = Metaball(vec3(1.0,0.0,0.0), vec2(100.0,90.0), 60.0);
+    metaballs[1] = Metaball(vec3(0.0,1.0,0.0), vec2(120.0,200.0), 50.0);
+    metaballs[2] = Metaball(vec3(0.0,0.0,1.0), vec2(200.0,90.0), 45.0);
+    metaballs[3] = Metaball(vec3(1.0,0.0,0.0), vec2(400.0,190.0), 45.0);
 
-    vec3 col = vec3(0, 0, 0);
+    metaballs[0].position.x = u_res.x*sin(u_time*1.2314)/2.0  + u_res.x/2.0;
+    metaballs[0].position.y = u_res.y*cos(u_time*1.2314)/2.0  + u_res.y/2.0;	
+    metaballs[1].position.x = u_res.x*sin(u_time*1.2314)/2.0  + u_res.x/2.0;
+
+    vec3 col = vec3(0.0, 0.0, 0.0);
     float infl = 0.0;
+
     for(int i = 0; i < 4; i++) {
       Metaball mb = metaballs[i];
       float currInfl = mb.radius * mb.radius;
@@ -54,10 +57,11 @@ const fragmentShader = `
     } 
 
     if (infl > threshold) 
-      col = normalize(col);
+      col = normalize(col); 
 
-    gl_FragColor.xyz = col;
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    col = mix(col, vec3(1.0), smoothstep(threshold+0.05, threshold, infl) * smoothstep(threshold-0.05, threshold, infl));
+
+    gl_FragColor = vec4(col, 1.0);
   }
 `;
 
