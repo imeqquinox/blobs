358ecff6e63ba9898cb49e608bafb71fea280ca2
Lewis Wilson <lewiswilson1998@hotmail.co.uk>
2026-03-30 21:22:49 +0100

    fix timer and resizing

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ document.body.appendChild(renderer.domElement);
 
 const uniforms = {
   u_time: { value: 0.0 },
-  u_res: { value: new THREE.Vector2(600, 420) },
+  u_res: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
   u_mouse: { value: new THREE.Vector2(300, 210) }
 }
 
@@ -26,7 +26,7 @@ const fragmentShader = `
   uniform vec2 u_res;
   uniform vec2 u_mouse; 
 
-  const float threshold = 0.95;
+  const float threshold = 0.9;
 
   struct Metaball {
     vec3 color;
@@ -41,9 +41,9 @@ const fragmentShader = `
     metaballs[2] = Metaball(vec3(0.0,0.0,1.0), vec2(200.0,90.0), 45.0);
     metaballs[3] = Metaball(vec3(1.0,0.0,0.0), vec2(400.0,190.0), 45.0);
 
-    metaballs[0].position.x = u_res.x*sin(u_time*1.2314)/2.0  + u_res.x/2.0;
-    metaballs[0].position.y = u_res.y*cos(u_time*1.2314)/2.0  + u_res.y/2.0;	
-    metaballs[1].position.x = u_res.x*sin(u_time*1.2314)/2.0  + u_res.x/2.0;
+    metaballs[0].position.x = u_res.x*sin(u_time*1.2314)/2.0 + u_res.x/2.0;
+    metaballs[0].position.y = u_res.y*cos(u_time*1.2314)/2.0 + u_res.y/2.0;	
+    metaballs[1].position.x = u_res.x*sin(u_time*1.2314)/2.0 + u_res.x/2.0;
 
     vec3 col = vec3(0.0, 0.0, 0.0);
     float infl = 0.0;
@@ -74,7 +74,21 @@ const material = new THREE.ShaderMaterial({
 const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), material);
 scene.add(mesh);
 
-function animate(time) {
-  renderer.render(scene, camera);
+const timer = new THREE.Timer();
+
+function resize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  uniforms.u_res = { value: new THREE.Vector2(window.innerWidth, window.innerHeight) }
 }
+
+window.addEventListener('resize', resize);
+
+function animate(time) {
+  timer.update(time);
+  const t = timer.getElapsed();
+  uniforms.u_time.value = t;
+
+  renderer.render(scene, camera);
+};
+
 renderer.setAnimationLoop(animate);
